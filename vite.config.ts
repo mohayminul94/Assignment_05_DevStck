d1a913f1f2a7bcd54c4036b1fe86e4672f800d07
Mohayminul Islam <mohayminul02@gmail.com>
2026-09-16 17:04:11 +0600

    fix: update vite base path and build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Assignment_05_DevStck/',
   plugins: [
     react(),
     tailwindcss(),
